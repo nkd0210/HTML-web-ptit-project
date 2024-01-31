47a56a8e2cc288364f6c5080d691dcd00d054103
Nguyen Kim Dung <kimdungvn52@gmail.com>
2024-01-31 13:33:37 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var open = document.querySelector('.open-btn');
+var modal = document.querySelector('.modal');
+var xBtn = document.querySelector('#x_btn');
+var closeBtn = document.querySelector('.footer__btn');
+
+open.onclick = function () {
+    modal.classList.remove('hide');
+}
+
+xBtn.onclick = function () {
+    modal.classList.add('hide');
+}
+
+closeBtn.onclick = function () {
+    modal.classList.add('hide');
+}
+
+
